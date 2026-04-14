65c909f6718b8c3ddb679d869d5caa33d7406626
TechWolfAir <fadhilfn04@gmail.com>
2026-04-14 14:05:44 +0700

    Change branded sign in page

--- app/(auth)/layouts/branded.tsx
+++ app/(auth)/layouts/branded.tsx
@@ -9,10 +9,10 @@ export function BrandedLayout({ children }: { children: ReactNode }) {
       <style>
         {`
           .branded-bg {
-            background-image: url('${toAbsoluteUrl('/media/images/2600x1600/1.png')}');
+            background-image: url('${toAbsoluteUrl('/media/images/p2tel/sipatel.png')}');
           }
           .dark .branded-bg {
-            background-image: url('${toAbsoluteUrl('/media/images/2600x1600/1-dark.png')}');
+            background-image: url('${toAbsoluteUrl('/media/images/p2tel/sipatel.png')}');
           }
         `}
       </style>
@@ -24,30 +24,6 @@ export function BrandedLayout({ children }: { children: ReactNode }) {
         </div>
 
         <div className="lg:rounded-xl lg:border lg:border-border lg:m-5 order-1 lg:order-2 bg-top xxl:bg-center xl:bg-cover bg-no-repeat branded-bg">
-          <div className="flex flex-col p-8 lg:p-16 gap-4">
-            <Link href="/">
-              <img
-                src={toAbsoluteUrl('/media/app/mini-logo.svg')}
-                className="h-[28px] max-w-none"
-                alt=""
-              />
-            </Link>
-
-            <div className="flex flex-col gap-3">
-              <h3 className="text-2xl font-semibold text-mono">
-                Secure Dashboard Access
-              </h3>
-              <div className="text-base font-medium text-secondary-foreground">
-                A robust authentication gateway ensuring
-                <br /> secure&nbsp;
-                <span className="text-mono font-semibold">
-                  efficient user access
-                </span>
-                &nbsp;to the Metronic
-                <br /> Dashboard interface.
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </>
